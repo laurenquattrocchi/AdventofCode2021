3aecb402f8dec01266703b1313dd3ebf3393b833
Lauren Brandon Quattrocchi <laurenq@linux5.cs.uchicago.edu>
2021-12-23 14:50:52 -0600

    day 1 part 2

--- day1.py
+++ day1.py
@@ -1,21 +1,41 @@
+#file to read in
+depths = []
+with open('input.csv') as f:
+    for line in f:
+        depth = line.strip()
+        depths.append(int(depth))
+
 #part 1
-def depth_increases(filename):
-    depths = []
-    with open(filename) as f:
-        for line in f:
-            depth = line.strip()
-            depths.append(int(depth))
-
-
+def depth_increases(lst):
     count = 0
-    for i, value in enumerate(depths):
+    for i, value in enumerate(lst):
         if i == 0:
             last_value = value
             continue
         if value > last_value:
             count += 1
         last_value = value
-
-    print(count)
+    return count
 
 #part 2
+def win_depth_increase(lst):
+    count = 0
+    for i,value in enumerate(lst):
+        if i + 2 > len(lst) - 1:
+            break
+        else:
+            win = []
+            win.append(value)
+            win.append(lst[i+1])
+            win.append(lst[i+2])
+            total = sum(win)
+            if i == 0:
+                last_total = total
+                continue
+            if total > last_total:
+                count += 1
+            last_total = total
+    return count
+
+
+
